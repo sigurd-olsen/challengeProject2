01febf77e87837aceafa72d34680260e2861e59d
sigurd-olsen <sigurddaaolsen@gmail.com>
2025-01-07 09:30:59 +0100

    added a for loop for case 3

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -287,14 +287,33 @@ do
 
         case "3":
             // Ensure animal ages and physical descriptions are complete
-            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            //Console.WriteLine("Challenge Project - please check back soon to see progress.");
+
+             for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    Console.WriteLine(ourAnimals[i,0]);
+                }
+            }
+
+
+
+
+
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
 
         case "4":
             // Ensure animal nicknames and personality descriptions are complete
-            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            // Console.WriteLine("Challenge Project - please check back soon to see progress.");
+
+           
+
+
+
+
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
